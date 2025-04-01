34c7364c6e1d4449c953170a0d4f4cd8549ff1d6
지안 <jian11300@gmail.com>
2025-04-01 11:51:17 +0900

    3교시

---
+++ yeonsung-python/3월 수업 내용/2025-04-01/2교시 - 자로형.py
@@ -0,0 +1,11 @@
+var1 = 100
+print(type(var1))
+
+var2 = 3.14
+print(type(var2))
+
+var3 = "hello"
+print(type(var3))
+
+var4 = True
+print(type(var4))
---
+++ yeonsung-python/3월 수업 내용/2025-04-01/len 함수.py
@@ -0,0 +1,10 @@
+s1: str = input("첫번재 문자열 입력: ")
+s2: str = input("두번재 문자열 입력: ")
+
+t1 = len(s1)
+t2 = len(s2)
+
+print(t1)
+print(t2)
+
+print(f"두 문자열의 길이 차이는 {t2 - t1} 입니다.")
