5c7d204ecaf146943d2f1d82b5e3ae7e924e821d
신지안 <75853990+shinjian@users.noreply.github.com>
2025-04-03 04:44:07 +0900

    3교시 - 확인 문제(단위변환) 리팩토링

--- yeonsung-python/3월 수업 내용/2025-03-25/3교시 - 확인 문제(단위변환).py
+++ yeonsung-python/3월 수업 내용/2025-03-25/3교시 - 확인 문제(단위변환).py
@@ -1,8 +1,14 @@
-POUND_TO_KG: float = 0.453592
-KG_TO_POUND: float = 2.204623
+POUND_TO_KG = 0.453592
+KG_TO_POUND = 2.204623
 
-pound: float = float(input("파운드 입력: "))
-print(f"{pound}는 {round(pound * POUND_TO_KG, 2)}kg")
+def pound_to_kg(pound: float) -> float:
+    return round(pound * POUND_TO_KG, 2)
 
-kg: float = float(input("킬로그램 입력: "))
-print(f"{kg}는 {round(kg * KG_TO_POUND, 2)}lb")
+def kg_to_pound(kg: float) -> float:
+    return round(kg * KG_TO_POUND, 2)
+
+pound = float(input("파운드 입력: "))
+print(f"{pound}lb 는 {pound_to_kg(pound)}kg 입니다.")
+
+kg = float(input("킬로그램 입력: "))
+print(f"{kg}kg 는 {kg_to_pound(kg)}lb 입니다.")
