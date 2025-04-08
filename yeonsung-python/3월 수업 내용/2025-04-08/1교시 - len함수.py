efdbd3003f6ff8139e73e895f2d974b2887f93da
지안 <jian11300@gmail.com>
2025-04-08 11:22:57 +0900

    .

---
+++ yeonsung-python/3월 수업 내용/2025-04-08/1교시 - len함수.py
@@ -0,0 +1,4 @@
+var1 = input("첫번째 문자열: ")
+var2 = input("두번째 문자열: ")
+
+print(f"문자열 길이 차이 : {len(var2) - len(var1)}")
---
+++ yeonsung-python/3월 수업 내용/2025-04-08/1교시 - uppper.py
@@ -0,0 +1,6 @@
+s1 = "Hello"
+
+print(s1.upper())
+print(s1.lower())
+
+print(s1.isupper())
